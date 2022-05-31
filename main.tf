f4e4eb6a047c589a1847280c5f5634866a444233
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2022-05-31 14:34:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_subscription_policy_exemption" "example" {
+  name = "exampleExemption"
+  #resource_group_id    = data.azurerm_subscription.example.id
+  subscription_id      = data.azurerm_subscription.current.id
+  policy_assignment_id = "/subscriptions/12f38dd9-8e4f-4697-96aa-c3d2ccd1793c/providers/Microsoft.Authorization/policyAssignments/190a335928b84a949fed473f"
+  exemption_category   = "Mitigated"
+}
